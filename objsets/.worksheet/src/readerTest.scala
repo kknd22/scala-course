a9049a9d990b616684519aebb3de1bb9fa4e0f29
kknd22 <chris.lin@mail.com>
2013-10-05 17:08:32 -0400

    added week 3

---
+++ objsets/.worksheet/src/readerTest.scala
@@ -0,0 +1,6 @@
+import objsets.TweetReader
+
+object readerTest {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(91); 
+  println("Welcome to the Scala worksheet")}
+
+}
---
+++ objsets/src/main/scala/readerTest.sc
@@ -0,0 +1,6 @@
+import objsets.TweetReader
+
+object readerTest {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+
+}
